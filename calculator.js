9dfaaefe690b78a6ddcdba053d6100f88f139d07
khmahfuzhasan <khandakermahfuzhasan@gmail.com>
2022-08-28 06:59:49 +0600

    Object Oriented Calculator Final Project

---
+++ calculator.js
@@ -0,0 +1,121 @@
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+    clear(){
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation =undefined;
+           
+    }
+    delete(){
+        if(this.currentOperand === '')  return;
+        this.currentOperand = this.currentOperand.toString().slice(0,-1);
+    }
+    appendNumber(number){
+        if(number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand += number.toString();
+        
+    }
+    chooseOperation(operation){
+        if(this.currentOperand === '')  return;
+        if(this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+     
+        
+    }
+
+    compute(){
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(current)) return;
+        switch(this.operation){
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+  
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+   
+    }
+    getDisplayNumber(number){
+        const floatNumber = parseFloat(number);
+        if(isNaN(floatNumber)) return '';
+        return floatNumber.toLocaleString('en');
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerText =  this.currentOperand;
+        this.getDisplayNumber(this.currentOperand);
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = `${this.previousOperand}${this.operation} `;
+            
+        }else{
+            this.previousOperandTextElement.innerText = '';
+        }
+        
+        
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+
+numberButtons.forEach(number =>{
+    number.addEventListener('click',() =>{
+        calculator.appendNumber(number.innerText);
+        calculator.updateDisplay();
+        
+    });
+});
+operationButtons.forEach(operation =>{
+    operation.addEventListener('click',() =>{
+        calculator.chooseOperation(operation.innerText);
+        calculator.updateDisplay();
+        
+    });
+});
+
+allClearButton.addEventListener('click',() =>{
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+
+equalsButton.addEventListener('click',() =>{
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+
+deleteButton.addEventListener('click',() =>{
+    calculator.delete();
+    calculator.updateDisplay();
+});
